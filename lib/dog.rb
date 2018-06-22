bac531ce0842a98e1ac743ed320b4b2ea7f68cf6
Megan Nepshinsky <mnepshinsky@gmail.com>
2018-06-22 03:19:33 +0000

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,15 @@
 # dog.rb
+
+class Dog
+    
+  def initialize
+end 
+
+end 
+
+fido = Dog.new 
+snoopy = Dog.new 
+lassie = Dog.new 
+
+
+
--- lib/person.rb
+++ lib/person.rb
@@ -1,1 +1,13 @@
 # person.rb
+
+class Person 
+  
+  def initalize 
+  end 
+
+end 
+
+
+adele_goldberg = Person.new 
+alan_kay = Person.new 
+
